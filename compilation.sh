3f4ca410669522bb590359b1a1aa8e97cd9ce4f5
Amia <hermionegranger3423@gmail.com>
2026-09-10 20:17:34 +0300

    Оновлено скрипт компіляції (додано дизасемблери)

--- compilation.sh
+++ compilation.sh
@@ -1,2 +1,4 @@
 gcc -O0 -g -fno-omit-frame-pointer -o matmult_O0 matmult.c
 gcc -O3 -march=native -g -fno-omit-frame-pointer -o matmult_O3native matmult.c
+gcc -O0 -S -masm=intel -fverbose-asm -o asm_O0.s matmult.c
+gcc -O3 -march=native -S -masm=intel -fverbose-asm -o asm_O3.s matmult.c
