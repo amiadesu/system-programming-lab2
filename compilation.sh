4723056b7fd2900b73edcdec0c389ff5b2b8c1c4
Amia <hermionegranger3423@gmail.com>
2026-09-10 20:20:59 +0300

    Перейменовано compilation.sh → build.sh

--- compilation.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-gcc -O0 -g -fno-omit-frame-pointer -o matmult_O0 matmult.c
-gcc -O3 -march=native -g -fno-omit-frame-pointer -o matmult_O3native matmult.c
-gcc -O0 -S -masm=intel -fverbose-asm -o asm_O0.s matmult.c
-gcc -O3 -march=native -S -masm=intel -fverbose-asm -o asm_O3.s matmult.c
